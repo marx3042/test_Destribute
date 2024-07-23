d1542e1f85145d52584f093f25ba21db91c3b9d0
marx3042 <marx3042@gmail.com>
2024-07-23 17:08:56 +0900

    first

---
+++ src/main/java/com/example/distributetest/DistributeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributeTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/distributetest/DistributeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
